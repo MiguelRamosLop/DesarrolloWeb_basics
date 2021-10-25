5f24dc788b70f5a8e5adc1883e83e9daa90796dd
Miguel Ramos López <miguelon2001b@gmail.com>
2021-10-25 12:14:15 +0200

    upload folder Intro a JS

---
+++ Clase 25.10.2021. Intro a JS/js/script.js
@@ -0,0 +1,11 @@
+console.log('Hola Mundo!!!!')
+let i, diez = "10"
+i = 10
+console.log(i)
+if (i % 2 == 0) 
+    console.log('Par')
+console.log(diez)
+if(i === diez)
+    console.log('Good')
+else 
+    console.log('Bad')
