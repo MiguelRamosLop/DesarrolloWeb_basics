9bc6c8dabb773f77f04242fbb95cc1ea7d6b1564
Miguel Ramos López <miguelon2001b@gmail.com>
2021-10-28 10:11:32 +0200

    upload folder 25.10

--- Clase 25.10.2021. Intro a JS/js/script.js
+++ Clase 25.10.2021. Intro a JS/js/script.js
@@ -9,3 +9,29 @@ if(i === diez)
     console.log('Good')
 else 
     console.log('Bad')
+
+let dias = new Array(2);
+
+dias[0] = 'lunes'
+dias[1] = 'martes'
+
+let htmlDias = "<ul>"
+for (let i = 0; i < dias.length; i++) {
+    htmlDias += "<li>"
+    htmlDias += dias[i]
+    htmlDias += "</li>"
+}
+htmlDias += "<ul>"
+
+
+//document.getElementById('semana').innerHTML = htmlDias
+
+//dame todos los tag name de tipo p pero coge solo el primer elemento
+document.getElementsByTagName('p')[0].innerHTML='<b>Loren Ipsum</b>'
+
+
+let h = document.createElement('h3')
+let t = document.createTextNode('Title with JS')
+h.appendChild(t)
+document.body.appendChild(h)
+//document.body.insertBefore(htmlDias, h) insertr antes, htmldias tiene que ser un node
